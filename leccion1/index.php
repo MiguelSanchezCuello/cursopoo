abbe97e26c40848debab502e0323ca486a57f861
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-07-28 20:42:28 -0400

    tomado del video de youtube:
    https://www.youtube.com/watch?v=PkPk1bWiaUw&list=PL3b9xmg86NTKWP3Xzu-1DCwaeO5sftK4V&index=5

--- leccion1/index.php
+++ leccion1/index.php
@@ -2,6 +2,7 @@
     require_once("ClassOperacion.php");
 
     $operacion1 = new Operacion(10, 2);
+    $operacion2 = new Operacion(100, 3);
 
     $totalSuma = $operacion1->getSuma();
     echo "Total de la suma: ".$totalSuma."<br />";
@@ -13,6 +14,20 @@
     echo "Total de la Division: ".$totalDivision."<br />";
 
     $totalMultiplicacion = $operacion1->getMultiplicacion();
-    echo "Total de la Multiplicacion: ".$totalMultiplicacion."<br />";
+    echo "Total de la Multiplicacion: ".$totalMultiplicacion."<br /><br />";
 
+
+    // Test con operacion2
+    $totalSuma = $operacion2->getSuma();
+    echo "Total de la suma: ".$totalSuma."<br />";
+
+    $totalResta = $operacion2->getResta();
+    echo "Total de la Resta: ".$totalResta."<br />";
+
+    $totalDivision = $operacion2->getDivision();
+    echo "Total de la Division: ".$totalDivision."<br />";
+
+    $totalMultiplicacion = $operacion2->getMultiplicacion();
+    echo "Total de la Multiplicacion: ".$totalMultiplicacion."<br /><br />";
+    
 ?>
