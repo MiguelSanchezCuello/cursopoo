1a189da608c1c44370b54c5e09601c992be09b11
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-07-28 22:40:20 -0400

    Otro ejemplo de como un cliente hereda de una clase persona superior.

--- herencia/Empresa.php
+++ herencia/Empresa.php
@@ -1,10 +1,19 @@
 <?php
     require_once("ClassEmpleado.php");
+    require_once("ClassCliente.php");
 
     $objEmpleado = new Empleado(00064, "Miguel Sanchez", 37);
+    $objCliente = new Cliente(00065, "Dawilka Arias", 29);
 
     echo $objEmpleado->getDatosPersonales();
-
     $objEmpleado->setPuesto("Administrador");
-
     echo "<br>".$objEmpleado->getPuesto();
+
+    echo "<br />".$objCliente->getDatosPersonales();
+    $objCliente->setCredito(18000.55);
+    echo "<br />".$objCliente->getCredito();
+
+
+    
+    
+
